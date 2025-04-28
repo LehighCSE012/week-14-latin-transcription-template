0dc27000da9e2fb13cb6109e8b5cb68b48332c73
Joshua Isom <110069928+JoshIsomLehigh@users.noreply.github.com>
2025-04-28 18:26:00 -0400

    Create transcription.py

---
+++ transcription.py
@@ -0,0 +1,19 @@
+def get_transcription(image_url: str) -> str:
+  return '''
+          Hec est finalis concordia facta in Curia domini Regis apud Westmonasterium in Octabis sancti Iohannis Baptiste Anno regnorum henrici
+        Regis Anglie et ffrancie sexti a conquestu decimo septimo coram Iohanne ffortescu et Jacobo Strang' Regis Justiciariis
+        passum Ricardo Neuton' et Thoma ffulthorp' Justiciariis Et postea in Octabis sancte Trinitatis Anno regnorum eiusdem
+        regis henrici decimo octauo ibidem concessa et recordata coram prefatis Ricardo Jacobo Strang' Thoma et Willelmo
+        paston' Justiciariis et aliis domini Regis fidelibus tunc ibi presentibus Inter Robertum Isham querentem et Thomam Alyngton'
+        deforciantem de vno mesuagio Centum acris terre duodecim acris prati quatuordecim acris pasture duodecim
+        denaratis redditus et redditu duodecim caponum et vnius libre piperis cum pertinenciis in parua bowdon'
+        que margareta que fuit uxor Thome alyngton' tenet ad terminum vite Vnde placitum conuencionis summonitum
+        fuit inter eos in eadem Curia Silicet quod predictus Thomas recognovit predicta tenementa et redditum cum pertinenciis esse jus ipsius
+        Roberti Et concessit pro se et heredibus suis quod predicta tenementa et redditus cum pertinenciis que predicta margareta tenet ad terminum
+        vite de hereditate predicti Thome in predicta Villa die quo hec concordia facta fuit Et que post decessum ipsius
+        margarete ad predictum Thomam et heredes suos debuerunt reverti post decessum ipsius margarete integre
+        remaneant predicto Roberto et heredibus suis Tenendum de Capitalibus dominis feodi illius per seruicia que ad predicta tenementa
+        et redditum pertinent imperpetuum Et predictus Thomas et heredes sui Warantizabunt predicto Roberto et heredibus suis predicta
+        tenementa et redditum cum pertinenciis contra omnes homines imperpetuum Et pro hac recognicione concessione Waranto fine et
+        concordia idem Robertus dedit predicto Thome Centum marcas argenti
+        '''
